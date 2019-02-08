63b1f7f1e1806276020e582a8fc94d654f7d3207
ScubbX <markusmayr@gmx.net>
2019-02-08 15:32:43 +0100

    added pypi necessary files

---
+++ setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="webguitest-scubbx",
+    version="0.0.1",
+    author="Markus Mayr",
+    author_email="markusmayr@gmx.net",
+    description="A web user interface test module combining Selenium and pyautogui into one interface ",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/scubbx/webguitest",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
